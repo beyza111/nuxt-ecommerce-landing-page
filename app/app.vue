8855e281c9b88bc3091e0580ad3b57f35ee76efa
beyza111 <beyzaoz203@gmail.com>
2025-11-18 16:47:06 +0300

    Setup basic app.vue structure

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container">
+    <h1> Welcome to my app</h1>
   </div>
 </template>
+
+<script setup>
+  // Javascript codes can be added here
+</script>
+
+<style>
+/*css codes can be added here */
+body{
+  font-family: sans-serif;
+  margin:0;
+  padding:0;
+}
+</style>
